d8b3ea93e39836320f837e22b50815f2f2e0d18c
nguy2940 <nhatle.nguyen@mail.utoronto.ca>
2023-05-15 10:40:30 +0700

    Project react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'; //for React
+import { BrowserRouter as Link } from 'react-router-dom';
 import './App.css';
+import "primereact/resources/themes/vela-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
+const headerstyle = {
+  backgroundColor: 'grey',
+  height: 70 
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='nav-bar' id='nav-bar' style={headerstyle}>
+        <header className='head-menu' id='header'>
+        </header>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/">About</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
